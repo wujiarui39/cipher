f019343226196356f4135237258cc8885db9b2cf
wujiarui39 <wujiarui1@foxmail.com>
2025-05-13 18:04:18 +0900

    co-op with jfc

--- main.py
+++ main.py
@@ -34,3 +34,12 @@ def decipher(text, shift_amount):
         elif c.isupper():
             unshifted += chr((ord(c) - ord('A') - shift_amount) % 26 + ord('A'))
     return unshifted
+
+if __name__ == '__main__':
+    original_text = get_original_text()
+    shift_amount = get_shift_amount()
+    text_letter_only = remove_nonletters(original_text)
+    cipher_text = cipher(text_letter_only, shift_amount)
+    print(f'cipher_text={cipher_text}')
+    decipher_text = decipher(cipher_text, shift_amount)
+    print(f'decipher_text={decipher_text}')
