81e2d8f2e88a189b3b2d797941d8d3dca49a4add
jingfancai <jingfancai603@gmail.com>
2025-05-13 17:53:44 +0900

    co-op with jr-w

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print('this code is written by jiarui wu')
+this is written by jingfan cai.
+
+import argparse
+import string
+
+def get_original_text():
+    with open('input.txt', 'r', encoding='utf-8') as f:
+        return f.read()
+def get_shift_amount():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--shift', type=int, default=1)
+    args = parser.parse_args()
+    return args.shift
